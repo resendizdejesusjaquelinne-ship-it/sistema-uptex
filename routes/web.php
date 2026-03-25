23e6b6e1f4698bda57396450be63fb283b1e5618
JAQUELINNE RESENDIZ DE JESUS <resendizdejesusjaquelinne@gmail.com>
2026-03-25 11:50:03 -0600

    Se agrega controlador, vistas y rutas para el CRUD de usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('usuarios', UsuarioController::class);
